d7bc3acce0643a7606667fd8315f184f73f6521d
Karl Anderson <kra@monkey.org>
2023-05-28 14:43:51 -0700

    add service

--- server.py
+++ server.py
@@ -1,6 +1,19 @@
-#!/usr/bin/env python
-import time
+import os
+import http.server
+import socketserver
 
-print("this is a log message from server")
-while True:
-    time.sleep(1)
+from http import HTTPStatus
+
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        msg = 'Hello! you requested %s' % (self.path)
+        self.wfile.write(msg.encode())
+
+
+port = 6000
+print('Listening on port %s' % (port))
+httpd = socketserver.TCPServer(('', port), Handler)
+httpd.serve_forever()
