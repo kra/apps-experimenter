6161b74fa24f83b83359858b8b22671a7843199d
Karl Anderson <kra@monkey.org>
2023-06-12 23:19:49 -0700

    chat tester

--- test_chat.py
+++ test_chat.py
@@ -6,12 +6,7 @@ import dotenv
 dotenv.load_dotenv()
 import chat
 
-lines = [
-    "Franz: Is anybody here?",
-    "97d2dfd2-cfb1-4efd-ae64-d02918ca7a47: hello?",
-    "4c36ec80-cd51-4e9f-b5ba-1776bafee738: This is weird!",
-    "Franz: who is this?",
-    "97d2dfd2-cfb1-4efd-ae64-d02918ca7a47: hello."]
+lines = open("lines", 'r').readlines()
 
 async def main():
     print(await chat.chat_line(lines))
